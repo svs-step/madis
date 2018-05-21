e74ddca50dff4b415415bb2f1d2498cd426c28c7
Donovan Bourlard <donovan.bourlard@biig.fr>
2018-05-22 00:07:57 +0200

    Initialize AdminLTE + Begin Collectivity CRUD

---
+++ src/Application/Controller/CRUDController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the SOLURIS - RGPD Management application.
+ *
+ * (c) Donovan Bourlard <donovan.bourlard@outlook.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Application\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+abstract class CRUDController extends Controller
+{
+}
--- src/Domain/User/Controller/CollectivityController.php
+++ src/Domain/User/Controller/CollectivityController.php
@@ -1,15 +1,70 @@
 <?php
 
+/**
+ * This file is part of the SOLURIS - RGPD Management application.
+ *
+ * (c) Donovan Bourlard <donovan.bourlard@outlook.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace App\Domain\User\Controller;
 
+use App\Domain\User\Form\Type\CollectivityType;
+use App\Domain\User\Model\Collectivity;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CollectivityController extends Controller
 {
-    public function listAction()
+    public function listAction(): Response
     {
-        return $this->render('User/Collectivity/list.html.twig');
+        $objects = $this->getDoctrine()->getRepository(Collectivity::class)->findAll();
+
+        return $this->render('User/Collectivity/list.html.twig', [
+            'objects' => $objects,
+        ]);
+    }
+
+    public function createAction(Request $request): Response
+    {
+        $object = new Collectivity();
+        $form   = $this->createForm(CollectivityType::class, $object);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($object);
+            $em->flush();
+
+            return $this->redirectToRoute('user_collectivity_list');
+        }
+
+        return $this->render('User/Collectivity/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function editAction(Request $request, string $id): Response
+    {
+        $object = $this->getDoctrine()->getRepository(Collectivity::class)->find($id);
+        $form   = $this->createForm(CollectivityType::class, $object);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($object);
+            $em->flush();
+
+            return $this->redirectToRoute('user_collectivity_list');
+        }
+
+        return $this->render('User/Collectivity/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
