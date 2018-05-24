7dbd1241d65c58320072addbe1e2028d2095cce1
Donovan Bourlard <donovan.bourlard@biig.fr>
2018-05-24 23:39:59 +0200

    Begin Dashboard

--- src/Domain/Reporting/Controller/DashboardController.php
+++ src/Domain/Reporting/Controller/DashboardController.php
@@ -13,12 +13,31 @@ declare(strict_types=1);
 
 namespace App\Domain\Reporting\Controller;
 
+use App\Domain\Admin\Model\Collectivity;
+use App\Domain\User\Model\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DashboardController extends Controller
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function indexAction()
     {
-        return $this->render('Reporting/Dashboard/index.html.twig');
+        $collectivities = $this->entityManager->getRepository(Collectivity::class)->findAll();
+        $users          = $this->entityManager->getRepository(User::class)->findAll();
+
+        return $this->render('Reporting/Dashboard/index.html.twig', [
+            'nbCollectivity' => \sizeof($collectivities),
+            'nbUser'         => \sizeof($users),
+        ]);
     }
 }
