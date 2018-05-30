a5c4f20ce9dc1e0d79a9d2e071abd121685fe1de
Donovan Bourlard <donovan.bourlard@biig.fr>
2018-05-31 00:10:47 +0200

    Enable usage of infrastructure

---
+++ src/Domain/User/Repository/User.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the SOLURIS - RGPD Management application.
+ *
+ * (c) Donovan Bourlard <donovan.bourlard@outlook.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\User\Repository;
+
+use App\Application\DDD\Repository\CRUDRepositoryInterface;
+
+interface User extends CRUDRepositoryInterface
+{
+}
