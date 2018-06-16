da6e678984074bd6275e57f0f3548865deb8e909
Donovan Bourlard <donovan.bourlard@biig.fr>
2018-06-16 17:40:24 +0200

    Add contractor list #14

---
+++ src/Domain/Registry/Form/Type/ContractorType.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the SOLURIS - RGPD Management application.
+ *
+ * (c) Donovan Bourlard <donovan.bourlard@outlook.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Registry\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ContractorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label'    => 'registry.contractor.form.name',
+                'required' => true,
+            ])
+        ;
+    }
+}
---
+++ tests/Domain/Registry/Model/ContractorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the SOLURIS - RGPD Management application.
+ *
+ * (c) Donovan Bourlard <donovan.bourlard@outlook.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Tests\Domain\Registry\Model;
+
+use App\Domain\Registry\Model\Contractor;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\UuidInterface;
+
+class ContractorTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $model = new Contractor();
+
+        $this->assertInstanceOf(UuidInterface::class, $model->getId());
+    }
+}
