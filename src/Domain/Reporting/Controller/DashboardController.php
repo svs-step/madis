781d767edfcd436e47aacaab5469851255138a0d
Donovan Bourlard <donovan.bourlard@biig.fr>
2018-06-28 23:08:54 +0200

    Add contractor in dashboard

--- src/Domain/Reporting/Controller/DashboardController.php
+++ src/Domain/Reporting/Controller/DashboardController.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace App\Domain\Reporting\Controller;
 
+use App\Application\Symfony\Security\UserProvider;
+use App\Domain\Registry\Model\Contractor;
 use App\Domain\User\Model\Collectivity;
 use App\Domain\User\Model\User;
 use Doctrine\ORM\EntityManagerInterface;
@@ -25,19 +27,59 @@ class DashboardController extends Controller
      */
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    /**
+     * @var UserProvider
+     */
+    private $userProvider;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UserProvider $userProvider
+    ) {
         $this->entityManager = $entityManager;
+        $this->userProvider  = $userProvider;
     }
 
     public function indexAction()
     {
+        $data = [
+            'collectivity' => 0,
+            'contractor'   => [
+                'clauses' => [
+                    'yes' => 0,
+                    'no'  => 0,
+                ],
+                'conform' => [
+                    'yes' => 0,
+                    'no'  => 0,
+                ],
+            ],
+            'user' => 0,
+        ];
+
         $collectivities = $this->entityManager->getRepository(Collectivity::class)->findAll();
+        $contractors    = $this->entityManager->getRepository(Contractor::class)->findBy(
+            ['collectivity' => $this->userProvider->getAuthenticatedUser()->getCollectivity()]
+        );
         $users          = $this->entityManager->getRepository(User::class)->findAll();
 
+        $data['collectivity'] = \count($collectivities);
+        foreach ($contractors as $contractor) {
+            if ($contractor->isContractualClausesVerified()) {
+                ++$data['contractor']['clauses']['yes'];
+            } else {
+                ++$data['contractor']['clauses']['no'];
+            }
+            if ($contractor->isConform()) {
+                ++$data['contractor']['conform']['yes'];
+            } else {
+                ++$data['contractor']['conform']['no'];
+            }
+        }
+        $data['user'] = \count($users);
+
         return $this->render('Reporting/Dashboard/index.html.twig', [
-            'nbCollectivity' => \sizeof($collectivities),
-            'nbUser'         => \sizeof($users),
+            'data' => $data,
         ]);
     }
 }
