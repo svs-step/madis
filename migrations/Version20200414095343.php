cd059b23979efeca553ba7a86437f36107ab449c
Alexis Thinardon <alexis.thinardon@agence-anode.fr>
2020-04-16 13:49:05 +0200

    Fix bad database migration

--- migrations/Version20200414095343.php
+++ migrations/Version20200414095343.php
@@ -23,7 +23,6 @@ final class Version20200414095343 extends AbstractMigration
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE registry_request ADD state VARCHAR(255) DEFAULT NULL, ADD state_rejection_reason VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE registry_treatment ADD concerned_people JSON NOT NULL COMMENT \'(DC2Type:json_array)\', DROP concerned_people_particular_check, DROP concerned_people_particular_comment, DROP concerned_people_user_check, DROP concerned_people_user_comment, DROP concerned_people_agent_check, DROP concerned_people_agent_comment, DROP concerned_people_elected_check, DROP concerned_people_elected_comment, DROP concerned_people_company_check, DROP concerned_people_company_comment, DROP concerned_people_partner_check, DROP concerned_people_partner_comment, DROP concerned_people_other_check, DROP concerned_people_other_comment');
     }
 
     public function down(Schema $schema): void
@@ -32,6 +31,5 @@ final class Version20200414095343 extends AbstractMigration
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE registry_request DROP state, DROP state_rejection_reason');
-        $this->addSql('ALTER TABLE registry_treatment ADD concerned_people_particular_check TINYINT(1) NOT NULL, ADD concerned_people_particular_comment VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD concerned_people_user_check TINYINT(1) NOT NULL, ADD concerned_people_user_comment VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD concerned_people_agent_check TINYINT(1) NOT NULL, ADD concerned_people_agent_comment VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD concerned_people_elected_check TINYINT(1) NOT NULL, ADD concerned_people_elected_comment VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD concerned_people_company_check TINYINT(1) NOT NULL, ADD concerned_people_company_comment VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD concerned_people_partner_check TINYINT(1) NOT NULL, ADD concerned_people_partner_comment VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD concerned_people_other_check TINYINT(1) NOT NULL, ADD concerned_people_other_comment VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, DROP concerned_people');
     }
 }
