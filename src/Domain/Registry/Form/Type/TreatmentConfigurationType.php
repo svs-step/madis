7827e07769fc7a79508a82829eb2dd8745035b96
tom caillaud <tom.caillaud@free.fr>
2021-07-09 12:10:02 +0200

    public registry treatment + administration

---
+++ src/Domain/Registry/Form/Type/TreatmentConfigurationType.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This file is part of the MADIS - RGPD Management application.
+ *
+ * @copyright Copyright (c) 2018-2019 Soluris - Solutions Numériques Territoriales Innovantes
+ * @author Donovan Bourlard <donovan@awkan.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Registry\Form\Type;
+
+use App\Domain\Registry\Model\PublicConfiguration;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
+
+class TreatmentConfigurationType extends AbstractType
+{
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * Build type form.
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $obj    = json_decode($options['data']->getSavedConfiguration(), true);
+        $mapped = (array) $options['data']->getMappedObject();
+
+        // merge initial object with saved values
+        $tabProperties = array_merge($mapped, $obj);
+
+        foreach ($tabProperties as $property => $value) {
+            $builder->add($property, CheckboxType::class, [
+                'label'    => 'registry.treatment.show.' . $this->fromCamelCase($property),
+                'required' => false,
+                'data'     => $value,
+            ]);
+        }
+    }
+
+    /**
+     * Provide type options.
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => PublicConfiguration::class,
+        ]);
+    }
+
+    protected function fromCamelCase($input)
+    {
+        $pattern = '!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!';
+        preg_match_all($pattern, $input, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ?
+                strtolower($match) :
+                lcfirst($match);
+        }
+
+        return implode('_', $ret);
+    }
+}
