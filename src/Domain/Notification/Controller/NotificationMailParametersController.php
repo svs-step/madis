8d99cc6207d4ff93c280ffd3c37ca9875c8a98ff
sebastien <dupuy@datakode.fr>
2022-04-19 14:44:05 +0200

    NotificationMailParameters controller edition for user ok

--- src/Domain/Notification/Controller/NotificationMailParametersController.php
+++ src/Domain/Notification/Controller/NotificationMailParametersController.php
@@ -41,6 +41,10 @@ use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+
+
+
+
 class NotificationMailParametersController extends CRUDController
 {
     /**
@@ -48,7 +52,12 @@ class NotificationMailParametersController extends CRUDController
      */
     protected $router;
 
-    public function __construct(EntityManagerInterface $entityManager, ControllerHelper $helper, RouterInterface $router,RequestStack $requestStack) {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ControllerHelper $helper,
+        RouterInterface $router,
+        RequestStack $requestStack
+    ) {
         $this->helper = $helper;
         $this->router = $router;
         $this->requestStack = $requestStack;
@@ -77,62 +86,89 @@ class NotificationMailParametersController extends CRUDController
 
     public function createNotifMailParams(string $id = null)
     {
+        if($id){
+            $notifMailParameters = $this->getDoctrine()->getRepository(Model\NotificationMailParameters::class)->find($id);
+
+            if (null === $notifMailParameters) {
+                throw new NotFoundHttpException("No object found with ID '{$id}'");
+            }
+            $form = $this->helper->createForm(NotificationMailParametersType::class, $notifMailParameters);
+        } else{
+            $form = $this->helper->createForm(NotificationMailParametersType::class);
+        }
 
         $request = $this->requestStack->getMasterRequest();
         $parameters = $request->request->all();
-
-        $form = $this->helper->createForm(NotificationMailParametersType::class);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             if (isset($id)){
-                $notifParams = $this->repository->findOneByID($id);
+                $notifParams = $notifMailParameters;
 
             } else {
                 $mailParams = $parameters['notification_mail_parameters'];
                 $notifParams = new Model\NotificationMailParameters();
-                if (isset($mailParams['is_notified'])){ $notifParams->setIsNotified($parameters['notification_mail_parameters']['is_notified']);}
-                $notifParams->setFrequency($parameters['alert']);
-
-                switch ($parameters['alert']){
-                    case 'every_hours':
-                        $notifParams->setIntervalHours(intval($parameters['alert']));
-                        break;
-                    case 'daily' :
-                        $notifParams->setStartHours(intval($parameters['daily_hour']));
-                        break;
-                    case 'weekly' :
-                        $notifParams->setStartHours(intval($parameters['weekly_hour']));
-                        $notifParams->setStartDay($parameters['weekly_day']);
-                        break;
-                    case 'monthly' :
-                        $notifParams->setStartHours(intval($parameters['monthly_hour']));
-                        $notifParams->setStartDay($parameters['monthly_day']);
-                        $notifParams->setStartWeek($parameters['monthly_week']);
-                        break;
-                }
-
-                if (isset($mailParams['is_treatment'])){ $notifParams->setIsTreatment($mailParams['is_treatment']);}
-                if (isset($mailParams['is_subcontract'])){$notifParams->setIsSubcontract($mailParams['is_subcontract']);}
-                if (isset($mailParams['is_request'])){$notifParams->setIsRequest($mailParams['is_request']);}
-                if (isset($mailParams['is_violation'])){$notifParams->setIsViolation($mailParams['is_violation']);}
-                if (isset($mailParams['is_proof'])){$notifParams->setIsProof($mailParams['is_proof']);}
-                if (isset($mailParams['is_protectAction'])){$notifParams->setIsProtectAction($mailParams['is_protectAction']);}
-                if (isset($mailParams['is_maturity'])){$notifParams->setIsMaturity($mailParams['is_maturity']);}
-                if (isset($mailParams['is_treatmenConformity'])){$notifParams->setIsTreatmenConformity($mailParams['is_treatmenConformity']);}
-                if (isset($mailParams['is_organizationConformity'])){$notifParams->setIsOrganizationConformity($mailParams['is_organizationConformity']);}
-                if (isset($mailParams['is_AIPD'])){$notifParams->setIsAIPD($mailParams['is_AIPD']);}
-                if (isset($mailParams['is_document'])){$notifParams->setIsDocument($mailParams['is_document']);}
-
-                $this->entityManager->persist($notifParams);
-                $this->entityManager->flush();
             }
+
+            if (isset($mailParams['is_notified'])){ $notifParams->setIsNotified($parameters['notification_mail_parameters']['is_notified']);}
+            $notifParams->setFrequency($parameters['alert']);
+
+            switch ($parameters['alert']) {
+                case 'every_hours':
+                    $notifParams->setIntervalHours(intval($parameters['alert']));
+                    break;
+                case 'daily' :
+                    $notifParams->setStartHours(intval($parameters['daily_hour']));
+                    break;
+                case 'weekly' :
+                    $notifParams->setStartHours(intval($parameters['weekly_hour']));
+                    $notifParams->setStartDay($parameters['weekly_day']);
+                    break;
+                case 'monthly' :
+                    $notifParams->setStartHours(intval($parameters['monthly_hour']));
+                    $notifParams->setStartDay($parameters['monthly_day']);
+                    $notifParams->setStartWeek($parameters['monthly_week']);
+                    break;
+            }
+
+            if (isset($mailParams['is_treatment'])){ $notifParams->setIsTreatment($mailParams['is_treatment']);}
+            if (isset($mailParams['is_subcontract'])){$notifParams->setIsSubcontract($mailParams['is_subcontract']);}
+            if (isset($mailParams['is_request'])){$notifParams->setIsRequest($mailParams['is_request']);}
+            if (isset($mailParams['is_violation'])){$notifParams->setIsViolation($mailParams['is_violation']);}
+            if (isset($mailParams['is_proof'])){$notifParams->setIsProof($mailParams['is_proof']);}
+            if (isset($mailParams['is_protectAction'])){$notifParams->setIsProtectAction($mailParams['is_protectAction']);}
+            if (isset($mailParams['is_maturity'])){$notifParams->setIsMaturity($mailParams['is_maturity']);}
+            if (isset($mailParams['is_treatmenConformity'])){$notifParams->setIsTreatmenConformity($mailParams['is_treatmenConformity']);}
+            if (isset($mailParams['is_organizationConformity'])){$notifParams->setIsOrganizationConformity($mailParams['is_organizationConformity']);}
+            if (isset($mailParams['is_AIPD'])){$notifParams->setIsAIPD($mailParams['is_AIPD']);}
+            if (isset($mailParams['is_document'])){$notifParams->setIsDocument($mailParams['is_document']);}
+
+            $this->entityManager->persist($notifParams);
+            $this->entityManager->flush();
+
+            //todo une fois que le modele est enregistré, revenir sur le user
         }
 
-        return $this->render('Notification/NotificationMailParameters/create.html.twig',
-            [
-                'form' => $form->createView(),
-            ]);
+        if (!isset($notifParams)){
+            return $this->render('Notification/NotificationMailParameters/create.html.twig',
+                [
+                    'form' => $form->createView()
+                ]);
+        } else{
+            return $this->render('Notification/NotificationMailParameters/create.html.twig',
+                [
+                    'form' => $form->createView(),
+                    'alert' => $notifParams->getFrequency(),
+                    'every_hours' => $notifParams->getIntervalHours(),
+                    'daily_hour' => $notifParams->getStartHours(),
+                    'weekly_hour' => $notifParams->getStartHours(),
+                    'weekly_day' => $notifParams->getStartDay(),
+                    'monthly_hour' => $notifParams->getStartHours(),
+                    'monthly_day' => $notifParams->getStartDay(),
+                    'monthly_week' => $notifParams->getStartWeek()
+                ]);
+        }
+
     }
 }
