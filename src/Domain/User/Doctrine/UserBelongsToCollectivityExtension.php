b06442bd085a4d0562ca183f53e97429769a1af1
Jonathan Foucher <jfoucher@gmail.com>
2022-05-12 11:02:28 +0200

    Only show collectivities for the current logged-in user. Fixes https://gitlab.adullact.net/soluris/madis/-/issues/490

---
+++ src/Domain/User/Doctrine/UserBelongsToCollectivityExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Domain\User\Doctrine;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Domain\User\Model\Collectivity;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
+
+final class UserBelongsToCollectivityExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = []): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (Collectivity::class !== $resourceClass || $this->security->isGranted('ROLE_ADMIN') || null === $user = $this->security->getUser()) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.id = :user_collectivity', $rootAlias));
+        $queryBuilder->setParameter('user_collectivity', $user->getCollectivity()->getId());
+    }
+}
