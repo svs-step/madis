c970255b3822098884f0f0dad2c9ee82b80f68bf
sebastien <dupuy@datakode.fr>
2022-09-30 11:03:09 +0200

    correctif php-cs-fixer

--- src/Infrastructure/ORM/Registry/Repository/Mesurement.php
+++ src/Infrastructure/ORM/Registry/Repository/Mesurement.php
@@ -376,7 +376,7 @@ class Mesurement extends CRUDRepository implements Repository\Mesurement
         // Fixes https://gitlab.adullact.net/soluris/madis/-/issues/529
         //$date         = new \DateTime();
         $queryBuilder = $this->createQueryBuilder();
-        $expr = $queryBuilder->expr();
+        $expr           = $queryBuilder->expr();
         $queryBuilder->select('u')
             ->from(Model\Mesurement::class, 'u')
             ->where($expr->neq('u.status', $expr->literal('not-applicable')))
