d6640876c270c0ff46d31783fd10f130e7172f0c
Jonathan Foucher <jfoucher@gmail.com>
2022-12-08 12:58:19 +0100

    Fix a few tests

--- tests/Domain/User/Component/MailerTest.php
+++ tests/Domain/User/Component/MailerTest.php
@@ -30,8 +30,13 @@ use App\Tests\Utils\ReflectionTrait;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class MailerTest extends TestCase
 {
@@ -39,7 +44,7 @@ class MailerTest extends TestCase
     use ProphecyTrait;
 
     /**
-     * @var \Swift_Mailer
+     * @var MailerInterface
      */
     private $mailerProphecy;
 
@@ -70,7 +75,7 @@ class MailerTest extends TestCase
 
     public function setUp(): void
     {
-        $this->mailerProphecy     = $this->prophesize(\Swift_Mailer::class);
+        $this->mailerProphecy     = $this->prophesize(MailerInterface::class);
         $this->translatorProphecy = $this->prophesize(TranslatorInterface::class);
         $this->twigProphecy       = $this->prophesize(Environment::class);
         $this->senderEmail        = 'foo@bar.baz';
@@ -92,24 +97,23 @@ class MailerTest extends TestCase
      */
     public function testSend()
     {
-        $this->mailerProphecy->send(Argument::type(\Swift_Message::class))->shouldBeCalled()->willReturn(1);
+        $this->mailerProphecy->send(Argument::type(Email::class))->shouldBeCalled();
 
-        $this->assertEquals(
-            1,
-            $this->invokeMethod(
-                $this->mailer,
-                'send',
-                ['to@gmail.com', 'subject', 'body']
-            )
+        $this->expectNotToPerformAssertions();
+        $this->invokeMethod(
+            $this->mailer,
+            'send',
+            ['to@gmail.com', 'subject', 'body']
         );
+
     }
 
     /**
      * Test sendForgetPassword.
      *
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function testSendForgetPassword()
     {
@@ -135,7 +139,7 @@ class MailerTest extends TestCase
             ->willReturn('dummy twig template')
         ;
 
-        $this->mailerProphecy->send(Argument::type(\Swift_Message::class))->shouldBeCalled()->willReturn(1);
+        $this->mailerProphecy->send(Argument::type(Email::class))->shouldBeCalled();
 
         $this->mailer->sendForgetPassword($userProphecy->reveal());
     }
--- tests/Functional/Symfony/EventSubscriber/Kernel/AccessModuleConformiteSubscriberTest.php
+++ tests/Functional/Symfony/EventSubscriber/Kernel/AccessModuleConformiteSubscriberTest.php
@@ -12,8 +12,10 @@ use Hautelook\AliceBundle\PhpUnit\RecreateDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
@@ -22,23 +24,54 @@ class AccessModuleConformiteSubscriberTest extends WebTestCase
 {
     use RecreateDatabaseTrait;
 
-    public function testDenyAccessToConformiteTraitementForNonAdminUsers()
+    public function testDenyAccessToConformiteTraitementForLecteurUsersOnCollectivityWithNoConformiteModule()
     {
         $client = static::createClient();
+        self::populateDatabase();
 
         // get or create the user somehow (e.g. creating some users only
         // for tests while loading the test fixtures)
         $userRepository = static::getContainer()->get(UserRepository::class);
-        $testUser = $userRepository->findOneOrNullByEmail('collectivite@awkan.fr');
-
-        dd($userRepository->findAll());
+        $testUser = $userRepository->findOneOrNullByEmail('lecteur@awkan.fr');
 
         $client->loginUser($testUser);
-        $url = $client->getContainer()->get('router')->generate('registry_conformite_traitement_list', array(), false);
+        $url = $client->getContainer()->get('router')->generate('registry_conformite_traitement_list', [], UrlGeneratorInterface::RELATIVE_PATH);
+        // user is now logged in, so you can test protected resources
+        $client->request('GET', $url);
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testAllowAccessToConformiteTraitementForLecteurUsersOnCollectivityWithConformiteModule()
+    {
+        $client = static::createClient();
+        self::populateDatabase();
+
+        // get or create the user somehow (e.g. creating some users only
+        // for tests while loading the test fixtures)
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneOrNullByEmail('reader-awkan@awkan.fr');
+
+        $client->loginUser($testUser);
+        $url = $client->getContainer()->get('router')->generate('registry_conformite_traitement_list', [], UrlGeneratorInterface::RELATIVE_PATH);
         // user is now logged in, so you can test protected resources
         $client->request('GET', $url);
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello Username!');
     }
 
+    public function testAllowAccessToConformiteTraitementForAdminUsers()
+    {
+        $client = static::createClient();
+        self::populateDatabase();
+
+        // get or create the user somehow (e.g. creating some users only
+        // for tests while loading the test fixtures)
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneOrNullByEmail('user_user_admin');
+
+        $client->loginUser($testUser);
+        $url = $client->getContainer()->get('router')->generate('registry_conformite_traitement_list', [], UrlGeneratorInterface::RELATIVE_PATH);
+        // user is now logged in, so you can test protected resources
+        $client->request('GET', $url);
+        $this->assertResponseIsSuccessful();
+    }
 }
