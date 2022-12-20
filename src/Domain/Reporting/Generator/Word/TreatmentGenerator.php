d4ddd301b6f90f8fedd175d8f14f4e3b9b72a85c
sebastien <dupuy@datakode.fr>
2022-12-20 10:13:13 +0100

    ajout des champs mentions legales, consentements apposées, et sous quel format.

--- src/Domain/Reporting/Generator/Word/TreatmentGenerator.php
+++ src/Domain/Reporting/Generator/Word/TreatmentGenerator.php
@@ -281,6 +281,18 @@ class TreatmentGenerator extends AbstractGenerator implements ImpressionGenerato
                         return TreatmentCollectingMethodDictionary::getMethods()[$cm];
                     }, $treatment->getCollectingMethod())) : '',
                 ],
+                7 => [
+                    'Mentions légales apposées',
+                    $treatment->getLegalMentions() ? 'Oui' : 'Non',
+                ],
+                8 => [
+                    'Consentement demandé',
+                    $treatment->getConsentRequest() ? 'Oui' : 'Non',
+                ],
+                9 => [
+                    'Format de la demande du consentement',
+                    $treatment->getConsentRequestFormat(),
+                ],
             ];
 
             // "Délai de conservation"
