caa333d63d817008cf44ecd3712149585d65c9b5
sebastien <dupuy@datakode.fr>
2022-12-30 09:14:00 +0100

    Preparation controleur referentiel

---
+++ src/Domain/Maturity/Controller/ReferentielController.php
@@ -0,0 +1,184 @@
+<?php
+
+/**
+ * This file is part of the MADIS - RGPD Management application.
+ *
+ * @copyright Copyright (c) 2018-2019 Soluris - Solutions Numériques Territoriales Innovantes
+ * @author Donovan Bourlard <donovan@awkan.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Maturity\Controller;
+
+use App\Application\Controller\CRUDController;
+use App\Application\Symfony\Security\UserProvider;
+use App\Domain\Maturity\Calculator\MaturityHandler;
+use App\Domain\Maturity\Form\Type\ReferentielType;
+use App\Domain\Maturity\Form\Type\SurveyType;
+use App\Domain\Maturity\Model;
+use App\Domain\Maturity\Repository;
+use App\Domain\Reporting\Handler\WordHandler;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Snappy\Pdf;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @property Repository\Referentiel $repository
+ */
+class ReferentielController extends CRUDController
+{
+    /**
+     * @var WordHandler
+     */
+    private $wordHandler;
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    protected $authorizationChecker;
+
+    /**
+     * @var UserProvider
+     */
+    protected $userProvider;
+
+    /**
+     * @var MaturityHandler
+     */
+    protected $maturityHandler;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        TranslatorInterface $translator,
+        Repository\Referentiel $repository,
+        WordHandler $wordHandler,
+        AuthorizationCheckerInterface $authorizationChecker,
+        UserProvider $userProvider,
+        MaturityHandler $maturityHandler,
+        Pdf $pdf
+    ) {
+        parent::__construct($entityManager, $translator, $repository, $pdf, $userProvider, $authorizationChecker);
+        $this->wordHandler          = $wordHandler;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->userProvider         = $userProvider;
+        $this->maturityHandler      = $maturityHandler;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDomain(): string
+    {
+        return 'maturity';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getModel(): string
+    {
+        return 'referentiel';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getModelClass(): string
+    {
+        return Model\Referentiel::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormType(): string
+    {
+        return ReferentielType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getListData()
+    {
+        $order = [
+            'createdAt' => 'DESC',
+        ];
+
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            return $this->repository->findAll($order);
+        }
+
+        if ($this->authorizationChecker->isGranted('ROLE_REFERENT')) {
+            $collectivities = \iterable_to_array($this->userProvider->getAuthenticatedUser()->getCollectivitesReferees());
+
+            return $this->repository->findAllByCollectivities($collectivities, $order);
+        }
+
+        $data = $this->repository->findAllByCollectivity(
+            $this->userProvider->getAuthenticatedUser()->getCollectivity(),
+            $order
+        );
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     * Here, we wanna compute maturity score.
+     *
+     * @param Model\Survey $object
+     */
+    public function formPrePersistData($object)
+    {
+        $this->maturityHandler->handle($object);
+    }
+
+    /**
+     * {@inheritdoc}
+     * Override method in order to hydrate survey answers.
+     */
+    public function createAction(Request $request): Response
+    {
+        /**
+         * @var Model\Survey
+         */
+        $modelClass = $this->getModelClass();
+        $object     = new $modelClass();
+
+        $form = $this->createForm($this->getFormType(), $object);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($object);
+            $em->flush();
+
+            $this->addFlash('success', $this->getFlashbagMessage('success', 'create', $object));
+
+            return $this->redirectToRoute($this->getRouteName('list'));
+        }
+
+        return $this->render($this->getTemplatingBasePath('create'), [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
---
+++ src/Domain/Maturity/Repository/Referentiel.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of the MADIS - RGPD Management application.
+ *
+ * @copyright Copyright (c) 2018-2019 Soluris - Solutions Numériques Territoriales Innovantes
+ * @author Donovan Bourlard <donovan@awkan.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\Domain\Maturity\Repository;
+
+use App\Application\DDD\Repository\CRUDRepositoryInterface;
+use App\Domain\User\Model\Collectivity;
+
+interface Referentiel extends CRUDRepositoryInterface
+{
+    /**
+     * Find all survey by associated collectivity.
+     *
+     * @param Collectivity $collectivity The collectivity to search with
+     * @param array        $order        Order the data
+     * @param int          $limit
+     *
+     * @return array The array of survey given by the collectivity
+     */
+    public function findAllByCollectivity(Collectivity $collectivity, array $order = [], int $limit = null): iterable;
+
+    /**
+     * Find previous survey by created_at date.
+     */
+    public function findPreviousById(string $id, int $limit = 1): iterable;
+
+    /**
+     * Average survey during the last year.
+     *
+     * @return string The count of mesurements
+     */
+    public function averageSurveyDuringLastYear(array $collectivites = []);
+
+    /**
+     * Find all survey by associated collectivities.
+     *
+     * @param array $collectivities The array of collectivity to search with
+     * @param array $order          Order the data
+     * @param int   $limit
+     *
+     * @return array The array of survey given by the collectivity
+     */
+    public function findAllByCollectivities(array $collectivities, array $order = [], int $limit = null): iterable;
+}
