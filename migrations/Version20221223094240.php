15abc446667eb4f122cf30ab220250b73f0d3855
Jonathan Foucher <jfoucher@gmail.com>
2023-01-17 12:07:39 +0100

    Fix violation nature migration (change current data) + Fix list of violations (show all natures)

--- migrations/Version20221223094240.php
+++ migrations/Version20221223094240.php
@@ -22,6 +22,8 @@ final class Version20221223094240 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
+        // Transform previous data to json array
+        $this->addSql('UPDATE registry_violation set violation_nature=CONCAT(\'["\', violation_nature,  \'"]\')');
         $this->addSql('ALTER TABLE registry_violation CHANGE violation_nature violation_natures JSON NOT NULL COMMENT \'(DC2Type:json_array)\'');
         $this->addSql('CREATE TABLE contractor_violation (violation_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', contractor_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', INDEX IDX_D2E7389E7386118A (violation_id), INDEX IDX_D2E7389EB0265DC7 (contractor_id), PRIMARY KEY(violation_id, contractor_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE contractor_violation ADD CONSTRAINT FK_D2E7389E7386118A FOREIGN KEY (violation_id) REFERENCES registry_violation (id) ON DELETE CASCADE');
--- src/Domain/Registry/Controller/ViolationController.php
+++ src/Domain/Registry/Controller/ViolationController.php
@@ -36,6 +36,7 @@ use App\Domain\Registry\Model;
 use App\Domain\Registry\Repository;
 use App\Domain\Reporting\Handler\WordHandler;
 use App\Domain\User\Dictionary\UserRoleDictionary;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Snappy\Pdf;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -180,10 +181,22 @@ class ViolationController extends CRUDController
                 ' . \date_format($violation->getDate(), 'd/m/Y') . '
             </a>';
 
+            $allNatures = ViolationNatureDictionary::getNatures();
+            $natures = '';
+            $naturesArray = new ArrayCollection($violation->getViolationNatures());
+
+            if (count($naturesArray) > 0)  {
+                $natures = $naturesArray->map(function($name) use ($allNatures) {
+                    return $allNatures[$name] ?? null;
+                })->filter(function($r) {return $r !== null;});
+
+                $natures = join(', ', $natures->toArray());
+            }
+
             $reponse['data'][] = [
                 'collectivite' => $violation->getCollectivity()->getName(),
                 'date'         => $violationLink,
-                'nature'       => !\is_null($violation->getViolationNatures()) ? ViolationNatureDictionary::getNatures() : null,
+                'nature'       => $natures,
                 'cause'        => !\is_null($violation->getCause()) ? ViolationCauseDictionary::getNatures()[$violation->getCause()] : null,
                 'gravity'      => !\is_null($violation->getGravity()) ? ViolationGravityDictionary::getGravities()[$violation->getGravity()] : null,
                 'actions'      => $this->getActionCellsContent($violation),
