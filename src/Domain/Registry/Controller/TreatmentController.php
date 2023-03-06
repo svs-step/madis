cfd34f31bf1e3334e39622edf0e7d27443759c7f
Jonathan Foucher <jfoucher@gmail.com>
2023-03-06 16:28:42 +0100

    Try to sort by sensitive data

--- src/Domain/Registry/Controller/TreatmentController.php
+++ src/Domain/Registry/Controller/TreatmentController.php
@@ -348,6 +348,10 @@ class TreatmentController extends CRUDController
 
         /** @var Model\Treatment $treatment */
         foreach ($treatments as $treatment) {
+            if (is_array($treatment)) {
+                $sensitiveData = $treatment['sensitiveData'];
+                $treatment = $treatment[0];
+            }
             if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_ANONYMOUSLY')) {
                 $treatmentLink = '<a href="' . $this->router->generate('registry_public_treatment_show', ['id' => $treatment->getId()->toString()]) . '">
                 ' . \htmlspecialchars($treatment->getName()) . '
@@ -390,10 +394,10 @@ class TreatmentController extends CRUDController
                 'specific_traitement'    => $this->getSpecificTraitement($treatment),
                 'conformite_traitement'  => $this->getTreatmentConformity($treatment),
                 'avis_aipd'              => $this->getAvisAipd($treatment),
-                'actions'                => $this->generateActionCellContent($treatment),
                 'exempt_AIPD'            => $treatment->getExemptAIPD() ? $yes : $no,
+                'statut'                 => $treatment->getStatut() && isset(TreatmentStatutDictionary::getStatuts()[$treatment->getStatut()]) ? TreatmentStatutDictionary::getStatuts()[$treatment->getStatut()] : '',
                 'sensitiveData'          => $this->countSensitiveData($treatment->getDataCategories()),
-                'statut'                 => TreatmentStatutDictionary::getStatuts()[$treatment->getStatut()],
+                'actions'                => $this->generateActionCellContent($treatment),
             ];
         }
 
