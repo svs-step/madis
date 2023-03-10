27438fac3a31c7607a658acf40ed90137b6d76f6
sébastien DUPUY <dupuy@datakode.fr>
2023-03-10 16:04:05 +0100

    Correction #605 [AIPD] Erreur 500 lors de l'import avec une mesure déjà existante. Remplacement de l uuid pour que cela crée la mesure.

--- src/Domain/AIPD/Controller/ModeleAnalyseController.php
+++ src/Domain/AIPD/Controller/ModeleAnalyseController.php
@@ -354,6 +354,13 @@ class ModeleAnalyseController extends CRUDController
             $serializer = SerializerBuilder::create()->build();
             $object     = $serializer->deserialize($content, ModeleAnalyse::class, 'xml');
             $object->deserialize();
+
+            foreach ($object->getScenarioMenaces() as $scenarioMenace) {
+                foreach($scenarioMenace->getMesuresProtections() as $mesuresProtection){
+                    $mesuresProtection->setIdFromString($this->guidv4());
+                }
+            }
+
             $object->setNom('(import) ' . $object->getNom());
             $this->entityManager->persist($object);
             $this->entityManager->flush();
@@ -378,5 +385,19 @@ class ModeleAnalyseController extends CRUDController
         ];
 
         return strtr($string, $unwanted_array);
+    }
+
+    function guidv4($data = null) {
+        // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+        $data = $data ?? random_bytes(16);
+        assert(strlen($data) == 16);
+
+        // Set version to 0100
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        // Set bits 6-7 to 10
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Output the 36 character UUID.
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }
