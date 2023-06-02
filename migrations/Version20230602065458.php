c933d4a8c7aecef4c2e9e8410cbf3c7104ec4f88
sebastien <dupuy@datakode.fr>
2023-06-02 09:35:31 +0200

    ajout du referentiel indice de maturité

---
+++ migrations/Version20230602065458.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230602065458 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('INSERT INTO maturity_referentiel(id, name, description, created_at, updated_at) VALUES (?, ?, ?, ?, ?)', [Uuid::uuid4(), 'Indice de maturité', null, date_create()->format('Y-m-d H:i:s'), date_create()->format('Y-m-d H:i:s')]);
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
