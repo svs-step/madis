8153b70cd2374bb500d1f8ef3b391b92364492ae
Jonathan Foucher <jfoucher@gmail.com>
2023-06-22 17:00:07 +0200

    cs -fix

--- migrations/Version20230622144206.php
+++ migrations/Version20230622144206.php
@@ -12,21 +12,21 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20230622144206 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
         $this->addSql('ALTER TABLE registry_treatment CHANGE legal_mentions legal_mentions TINYINT(1)  DEFAULT 0');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
     }
 }
