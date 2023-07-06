d6dd2c2ebda57eaf380a3f72756debb1a5b3b4c5
Jonathan Foucher <jfoucher@gmail.com>
2023-07-06 10:42:18 +0200

    change instead of rename column

--- migrations/Version20230615143837.php
+++ migrations/Version20230615143837.php
@@ -22,7 +22,7 @@ final class Version20230615143837 extends AbstractMigration
         $this->addSql('ALTER TABLE user_collectivity ADD COLUMN nbr_cnil2 VARCHAR(255);');
         $this->addSql('UPDATE user_collectivity SET nbr_cnil2 = CAST(nbr_cnil AS CHAR)');
         $this->addSql('ALTER TABLE user_collectivity DROP COLUMN `nbr_cnil`;');
-        $this->addSql('ALTER TABLE user_collectivity RENAME COLUMN `nbr_cnil2` to `nbr_cnil`;');
+        $this->addSql('ALTER TABLE user_collectivity CHANGE `nbr_cnil2` `nbr_cnil` VARCHAR(255);');
     }
 
     public function down(Schema $schema): void
