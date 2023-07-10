b5e77983d9e684ba422a03aafeb928dfe82bea58
sebastien <dupuy@datakode.fr>
2023-07-10 14:59:56 +0200

    Correctif pour la longueur de champ pour la description d'un ref, ajout du champ description pour une section, update de ultimate_fate à destruction pour les ultimate fate null dans les registre traitements shelf lifes

---
+++ migrations/Version20230710121656.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230710121656 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE maturity_referentiel CHANGE description description LONGTEXT DEFAULT NULL');
+        $this->addSql('UPDATE registry_treatment_shelf_life SET ultimate_fate = "destruction" where ultimate_fate IS NULL');
+        $this->addSql('ALTER TABLE maturity_domain ADD description LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE maturity_referentiel CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE maturity_domain DROP description');
+    }
+}
