217c5b01e1e1c0ad8310b1983c094d508c9dee8e
Jonathan Foucher <jfoucher@gmail.com>
2023-07-18 10:45:57 +0200

    Add migration to change option to optional if not already done.

---
+++ migrations/Version20230718084209.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230718084209 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->skipIf(
+            $schema->getTable('maturity_question')->hasColumn('optional'),
+            'Skipping because `optional` exists'
+        );
+        $this->addSql('ALTER TABLE maturity_question CHANGE `option` `optional` INT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    }
+}
