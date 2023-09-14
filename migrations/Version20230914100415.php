e27e5683480c64c0d5e5845688964b0309489c4e
sebastien <dupuy@datakode.fr>
2023-09-14 13:20:30 +0200

    ajout de la categorie indice de maturite

---
+++ migrations/Version20230914100415.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Ramsey\Uuid\Uuid;
+
+final class Version20230914100415 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'add category violation';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('INSERT INTO category(id, name, systeme, created_at, updated_at) VALUES (?, ?, ?, ?, ?)', [Uuid::uuid4(), 'Indice de maturité', true, date_create()->format('Y-m-d H:i'), date_create()->format('Y-m-d H:i')]);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
