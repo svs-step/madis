e9f30fb4e3328eac58968a733c22d7e107cdd92f
Michael Edlich <m.edlich@soluris.fr>
2024-01-08 17:35:38 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,9 @@ project = 'Madis'
 copyright = 'Madis Soluris'
 author = 'Soluris'
 language = "fr"
+html_theme = 'sphinx_book_theme'
+html_logo = "_images/Madis-Logo.png"
+html_title = "Documentation Madis"
 #html_theme = "sphinx_rtd_theme"
 #html_theme_options = {
 #    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
@@ -32,23 +35,10 @@ language = "fr"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-
-html_theme = 'sphinx_book_theme'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 # main file
-master_doc = 'index'
+master_doc = 'docutilisateur'
