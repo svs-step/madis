d5f5ebef1eef29f536d432e71b744b4c5b582691
Michael Edlich <m.edlich@soluris.fr>
2024-01-08 17:50:03 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,9 +11,27 @@ project = 'Madis'
 copyright = 'Madis Soluris'
 author = 'Soluris'
 language = "fr"
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 html_logo = "_images/Madis-Logo.png"
 html_title = "Documentation Madis"
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+
 #html_theme = "sphinx_rtd_theme"
 #html_theme_options = {
 #    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
@@ -32,13 +50,4 @@ html_title = "Documentation Madis"
 #    'includehidden': True,
 #    'titles_only': False
 #}
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# main file
-master_doc = 'docutilisateur'
