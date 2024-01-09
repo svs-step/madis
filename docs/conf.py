6ec88cf842cc670cd48995e234f5c8dcc2998f2c
Michael Edlich <m.edlich@soluris.fr>
2024-01-09 17:23:18 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,17 +12,11 @@ copyright = 'Madis Soluris'
 author = 'Soluris'
 language = "fr"
 html_theme = 'sphinx_rtd_theme'
-html_logo = "_images/Madis-Logo.png"
+#html_logo = "_images/Madis-Logo.png"
 html_title = "Documentation Madis"
 html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
