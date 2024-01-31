98b8677a01e869375c9df6094dfa6dbe18310b4f
Théo BOYER <t.boyer@si17.lan>
2024-01-31 12:13:02 +0100

    Fix Controller Journalisation Traduction

--- src/Domain/Reporting/Controller/JournalisationController.php
+++ src/Domain/Reporting/Controller/JournalisationController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class JournalisationController extends AbstractController
 {
@@ -28,8 +29,9 @@ class JournalisationController extends AbstractController
      */
     private $logJournalLinkGenerator;
 
-    public function __construct(LogJournal $logRepository, RouterInterface $router, LogJournalLinkGenerator $logJournalLinkGenerator)
+    public function __construct(LogJournal $logRepository, TranslatorInterface $translator, RouterInterface $router, LogJournalLinkGenerator $logJournalLinkGenerator)
     {
+        $this->translator              = $translator;
         $this->repository              = $logRepository;
         $this->router                  = $router;
         $this->logJournalLinkGenerator = $logJournalLinkGenerator;
